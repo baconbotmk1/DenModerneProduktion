aa48f47fc4d19d3d21549185641cd657e955c684
Jespercal <chap600@hotmail.com>
2025-04-22 00:11:21 +0200

    Much more functional now

---
+++ DenModerneProduktion/Components/Helpers/ModalInput.cs
@@ -0,0 +1,33 @@
+﻿namespace DenModerneProduktion.Components.Helpers
+{
+    public class ModalInput
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public InputType DType { get; set; }
+        public List<Option>? Options { get; set; }
+        public object? Value { get; set; }
+
+        public string? Error { get; set; } = null;
+    }
+
+    public enum InputType
+    {
+        Text,
+        Select,
+        Number,
+        Date
+    }
+
+    public class Option
+    {
+        public string Value { get; set; }
+        public string Label { get; set; }
+
+        public Option(string label, string? value = null)
+        {
+            Label = label;
+            Value = value != null ? value : label;
+        }
+    }
+}
