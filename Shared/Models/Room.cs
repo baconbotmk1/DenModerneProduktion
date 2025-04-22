79d30aad83d5c5ea315734bfc1bba92960987613
sa <sa@consortio-support.dk>
2025-04-22 14:20:49 +0200

    Tilføjet lokaler fra database til lokale oversigt og lobby skærm

--- Shared/Models/Room.cs
+++ Shared/Models/Room.cs
@@ -2,18 +2,19 @@
 using System.ComponentModel.DataAnnotations;
 namespace Shared.Models
 {
-	public class Room : BaseModel
-	{
+    public class Room : BaseModel
+    {
         [Required]
         public string Name { get; set; }
 
         [Required]
         public int SectionId { get; set; }
-		public Section Section { get; set; }
+        public Section Section { get; set; }
 
-		public Room()
-		{
-		}
-	}
+
+        public Room()
+        {
+        }
+    }
 }
 
