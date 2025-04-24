ce213e1013d979b53e43123be021b800091d7099
sa <sa@consortio-support.dk>
2025-04-24 13:10:38 +0200

    Nullable feltet på data limits

--- Shared/Models/DeviceDataLimitValue.cs
+++ Shared/Models/DeviceDataLimitValue.cs
@@ -9,14 +9,14 @@ namespace Shared.Models
         public int TypeId { get; set; }
         public DeviceDataType Type { get; set; }
         
-        public int RoomId { get; set; }
-        public Room Room { get; set; }
-        public int SectionId { get; set; }
-        public Section Section { get; set; }
-        public int BuildingId { get; set; }
-        public Building Building { get; set; }
-        public int CadastreId { get; set; }
-        public Cadastre Cadastre { get; set; }
+        public int? RoomId { get; set; }
+        public Room? Room { get; set; }
+        public int? SectionId { get; set; }
+        public Section? Section { get; set; }
+        public int? BuildingId { get; set; }
+        public Building? Building { get; set; }
+        public int? CadastreId { get; set; }
+        public Cadastre? Cadastre { get; set; }
 
 
         public DeviceDataLimitValue()
