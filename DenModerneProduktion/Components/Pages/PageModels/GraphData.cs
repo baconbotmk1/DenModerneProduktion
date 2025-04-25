19358d65ca665aec00d85137e753ce1abfc693ce
Sebastian Arnergaard <sebastian257@hotmail.dk>
2025-04-25 23:57:56 +0200

    Grænseværdi for rum, farvelægger felter udenfor grænseværdi, grupperet data.

---
+++ DenModerneProduktion/Components/Pages/PageModels/GraphData.cs
@@ -0,0 +1,9 @@
+﻿namespace DenModerneProduktion.Components.Pages.PageModels
+{
+    public class GraphData
+    {
+        public double Value { get; set; }
+        public string Timestamp { get; set; }
+        public string Color { get; set; }
+    }
+}
